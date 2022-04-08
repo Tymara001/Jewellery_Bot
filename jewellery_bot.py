7fcfc8c65cf2ba750fa98db16cde6636a6841d78
Tymara001 <99365765+Tymara001@users.noreply.github.com>
2022-04-08 14:40:36 +1200

    Added Welcome function
    Added welcome and main function from component 1 version 2

--- jewellery_bot.py
+++ jewellery_bot.py
@@ -1,11 +1,24 @@
 # Jewellery bot program
+import random
+from random import randint
+
+# List fo random names
+names = ["Rose", "Lily", "Eric", "Lyle", "Bella", "James", "Isabella", "katie", "Jack", "Lara"]
 
 # Welcome message with random name 
+def welcome():
+    '''
+ Purpose: To generate a random name from the list and print out
+ a welcome message
+ Parameters: None
+ Returns: None
+    '''
+    num = randint (0,9)
+    name = (names[num])
 
-
-
-
-
+    print("***** Welcome to Jules Jewellery *****")
+    print("***** My name is", name, "*****")
+    print("***** I will be here to help you order your wonderful Jules Jewellery *****") 
 
 # Menu for pick up or delivery
 
@@ -67,3 +80,14 @@
 
 
 #Main function
+def main():
+    '''
+ Purpose: To run all functions 
+ a welcome message
+ Parameters: None
+ Returns: None
+    '''
+    welcome()
+
+
+main()
