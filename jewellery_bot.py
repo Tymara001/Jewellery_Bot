87bedddf05ad2f486497ed67ce7c0521d4f11c9d
Tymara001 <99365765+Tymara001@users.noreply.github.com>
2022-04-08 14:30:46 +1200

    Added comments for each section
    Comments for each section

--- jewellery_bot.py
+++ jewellery_bot.py
@@ -1,2 +1,69 @@
 # Jewellery bot program
 
+# Welcome message with random name 
+
+
+
+
+
+
+# Menu for pick up or delivery
+
+
+
+
+
+# Pick up information - name and phone number 
+
+
+
+
+
+
+# Delivery information - name adress and phone 
+
+
+
+
+
+# Choose total number of accessories - max 10
+
+
+
+
+
+
+
+# Jewellery menu
+
+
+
+
+
+
+#Jewellery order - from menu - print each jewellery out with cost
+
+
+
+
+
+#Prin order out - including if order is del or pick up and names and price of each jewellery - total cost including any delivery charge
+
+
+
+
+#Ability to cancel or proceed with order
+
+
+
+
+
+
+#Option for new order or to exit
+
+
+
+
+
+
+#Main function
